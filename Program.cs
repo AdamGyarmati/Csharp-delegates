c0cf44788743329f6e69bac27bfb67db23dd4559
gyarmatiadam <gyari12@gmail.com>
2024-08-17 10:20:38 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace delegates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConnectToDatabase(PrintToFile);
+    }
+
+    // static void Print(string text)
+    // {
+    //     System.Console.WriteLine(text);
+    // }
+
+    static void ConnectToDatabase(PrintDelegate log)
+    {
+        // Do insertion
+        log("Insertin data to database.");
+        // Insertion done
+        log("The record got inserted into the database.");
+    }
+
+    static PrintDelegate PrintConsole = (string text) =>
+    {
+        Console.WriteLine(text);
+    };
+
+    static PrintDelegate PrintToFile = (string text) =>
+    {
+        File.AppendAllText("./logs.txt", text);
+    };
+
+    delegate void PrintDelegate(string text);
+}
